dc20b765f7d9ee092ea10907419f82ef68304003
LihaoHuang <azxaxa@gmail.com>
2016-03-16 02:36:30 +0800

    重複 function view($page = 'show') 的工作
    tag:
    1. 我讓 function view($page = 'show') 預設值是傳到 /show
    2. 但是如果有其他畫面，例如 : /create   或是  /login ，一樣是進到這個 function view ( $page ) 裡面
    3. 進到 view 這個方法中就會自動載入該頁面(因為$page = 你網址輸入的 value )，
      $this->load->view('templates/header');
      $this->load->view('pages/'.$page,$data);
      $this->load->view('templates/footer');

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -57,11 +57,4 @@ class Pages extends CI_Controller {
             redirect('show');
         }
     }
-
-    public function login()
-    {
-        $this->load->view('templates/header');
-        $this->load->view('pages/login');
-        $this->load->view('templates/footer');
-    }
 }
